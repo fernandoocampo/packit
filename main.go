378c011c598c6cdc6c08d32f9e98ff4325ecfd54
fernando ocampo <fdocampo@gmail.com>
2022-08-26 19:33:13 +0200

    add version to see ldflags working

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
+import "github.com/fernandoocampo/packit/version"
+
 // PrintHello print hello using the buildin function println().
 func PrintHello() {
-	println("hello")
+	println("hello", version.Version)
 }
 
 func main() {
