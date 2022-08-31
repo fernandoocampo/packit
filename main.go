6ee93c0a7b1fc46dc631977628494697a1cc20fe
fernando ocampo <fdocampo@gmail.com>
2022-08-31 22:07:36 +0200

    add logic to print os data

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/fernandoocampo/packit/version"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/fernandoocampo/packit/version"
+	"golang.org/x/sys/unix"
+)
 
 // PrintHello print hello using the buildin function println().
 func PrintHello() {
@@ -9,4 +15,23 @@ func PrintHello() {
 
 func main() {
 	PrintHello()
+	uname := unix.Utsname{}
+	unix.Uname(&uname)
+	fmt.Println(arrayToString(uname.Nodename))
+	fmt.Println(arrayToString(uname.Release))
+	fmt.Println(arrayToString(uname.Sysname))
+	fmt.Println(arrayToString(uname.Version))
+	fmt.Println(arrayToString(uname.Machine))
+}
+
+func arrayToString(x [256]byte) string {
+	var buf [256]byte
+	for i, b := range x {
+		buf[i] = byte(b)
+	}
+	str := string(buf[:])
+	if i := strings.Index(str, "\x00"); i != -1 {
+		str = str[:i]
+	}
+	return str
 }
